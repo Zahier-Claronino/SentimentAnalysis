a52245da51d5df8bc7a36af0bbb1bd47e4d516c1
Zahier Claronino <151405894+Zahier-Claronino@users.noreply.github.com>
2025-07-18 15:04:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.post('/analyse', async (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Proxy server running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
